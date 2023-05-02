d7501ebae07b906ac6a198a9df1d4f276b2e45e6
Marcos Paulo <126702767+MARC070W2@users.noreply.github.com>
2023-05-02 20:11:13 -0300

    Create conversão_temperatura.py

---
+++ conversão_temperatura.py
@@ -0,0 +1,7 @@
+''' faça um programa que peça a temperatura em graus celsius, transforme e mostre em graus farenheit '''
+
+c=float(input("enter the temperature in degrees celsius: "));
+
+f=((9*c)/5)+32;
+
+print("the temperature in degrees farenheit is: ",f)
