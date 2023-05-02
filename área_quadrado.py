1d74b146994404e3d6038c651a42cf0ca1841fc6
Marcos Paulo <126702767+MARC070W2@users.noreply.github.com>
2023-05-02 20:30:27 -0300

    Create área_quadrado.py

---
+++ área_quadrado.py
@@ -0,0 +1,11 @@
+''' faça um programa que calcule a area de um quadrado, em seguida mostre o dobro desta area ao usuario '''
+
+s = int(input("inform the value of the side: "))
+
+area = s**2;
+
+print("this area is: ",area);
+
+double_the_area = area*2;
+
+print("twice the area is: ",double_the_area)
