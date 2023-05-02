a096b7260bb38993b7b3c5b0487c0ce93a473657
Marcos Paulo <126702767+MARC070W2@users.noreply.github.com>
2023-05-02 00:34:18 -0300

    Create média_escolar.py

---
+++ média_escolar.py
@@ -0,0 +1,10 @@
+''' faça um programa que peça as 4 notas bimestrais e depois calcule a media ''' 
+
+n1=float(input("enter the first note: "));
+n2=float(input("enter the second note: "));
+n3=float(input("enter the third note: "));
+n4=float(input("enter the fourth note: "));
+
+average= (n1+n2+n3+n4)/4;
+
+print("the result gave: ", average)
