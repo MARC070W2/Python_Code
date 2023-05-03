a7b3654ad1ce7bc46f04a438b850802b6df3fd73
Marcos Paulo <126702767+MARC070W2@users.noreply.github.com>
2023-05-03 20:05:03 -0300

    Create soma_de_numeros.py

---
+++ soma_de_numeros.py
@@ -0,0 +1,9 @@
+''' faça um programa que peça 2 números e imprima a soma '''
+
+a = int(input("say a number: "));
+
+b = int(input("say a other number: "));
+
+sum = a + b;
+
+print("the result is: ",sum)
